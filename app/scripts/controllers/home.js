9fd59cd0574990b8239bc4fd34f680418cd97c97
Josh Hamilton <nearbycoder@gmail.com>
2015-07-08 11:44:58 -0500

    added css animation as well as skills widget

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,9 +8,15 @@
  * Controller of the ngBlogApp
  */
 angular.module('ngBlogApp')
-  .controller('HomeCtrl', function () {
+  .controller('HomeCtrl', function ($scope) {
     var theater = new TheaterJS();
     var count = 0;
+    $scope.ruby = '0%';
+  	$scope.angular = '0%';
+  	$scope.php = '0%';
+  	$scope.javascript = '0%';
+  	$scope.sass = '0%';
+  	$scope.css = '0%';
     var theaterRun = document.cookie.split('=')[1];
     if(theaterRun !== 'True'){
 	    theater
@@ -31,6 +37,15 @@ angular.module('ngBlogApp')
 	    		count++;
 	    		if(count === 4){
 	    			$('.my-info').fadeIn(1000);
+	    			$('.skills').fadeIn(1200, function(){
+	    				$scope.ruby = '80%';
+				  		$scope.angular = '60%';
+					  	$scope.php = '85%';
+					  	$scope.javascript = '67%';
+					  	$scope.sass = '50%';
+					  	$scope.css = '90%';
+					  	$scope.$apply();
+	    			});
 	    		}
 
 	    	})
@@ -41,6 +56,16 @@ angular.module('ngBlogApp')
 	  	$('#nearbycoder').append('I am a web developer with a passion to code. Currently I work with ruby on rails, php, and javascript frameworks such as angularjs');
 	  	$('#logo').append('Nearby&lt;/&gt;Coder');
 	  	$('.my-info').fadeIn(1000);
+	  	$('.skills').fadeIn(2000, function(){
+	  		$scope.ruby = '80%';
+	  		$scope.angular = '60%';
+		  	$scope.php = '85%';
+		  	$scope.javascript = '67%';
+		  	$scope.sass = '50%';
+		  	$scope.css = '90%';
+		  	$scope.$apply();
+	  	});
+	  	
 	  }
 
     document.cookie='theaterRun=True';
